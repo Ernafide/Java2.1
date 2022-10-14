c16658b082ef235c19563c8a2af75fa2deea3956
Julia <Ernafide@gmail.com>
2022-10-14 16:00:45 +0300

    Main

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 7600; // стоимость билета
+        int rubleMile = 20; // количество рублей за милю
+        int mile = ticketPrice / rubleMile; // колличество начисленных миль
+        System.out.println("Количество миль -" + mile );
+
+    }
+}
